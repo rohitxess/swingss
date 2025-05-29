30e1e81b492a46254f3f53366bfe7ecfe7b39491
rohitxess <xessro@gmail.com>
2025-05-29 19:10:06 +0530

    added code for the logout function

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Link from "next/link";
 import { AuthProvider } from "@/context/AuthContext";
 import Head from './head'
+import Logout from "@/components/Logout";
 
 const opensans = Open_Sans({ subsets: ["latin"] });
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
@@ -19,9 +20,7 @@ export default function RootLayout({ children }) {
       <Link href={'/'}>
         <h1 className={"text-base sm:text-lg textGradient " + fugaz.className}>Swingss</h1>
       </Link>
-      <div className="flex items-center justify-between">
-        PLACEHOLDER CTA || STATS
-      </div>
+      <Logout />
     </header>
   )
 
--- components/Logout.jsx
+++ components/Logout.jsx
@@ -1,12 +1,12 @@
 'use client'
 import { useAuth } from "@/context/AuthContext"
 import Button from "./Button"
-import { useAuth } from '@/context/AuthContext'
 import { usePathname  } from "next/navigation"
 import Link from 'next/link'
 
 export default function Logout() {
     const { logout, currentUser } = useAuth()
+    const pathname = usePathname()
 
     if(!currentUser){
         return null 
