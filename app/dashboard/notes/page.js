ea5581ca820450b5eefed761252dc452a7f2bc7d
rohitxess <xessro@gmail.com>
2025-05-31 17:50:41 +0530

    added a notes component

---
+++ app/dashboard/notes/page.js
@@ -0,0 +1,17 @@
+
+import Main from "@/components/Main";
+import Notes from "@/components/Notes";
+
+export const metadata =  {
+    title: 'Swingss . Notes'
+}
+
+export default function NotesPage() {
+
+        return (
+            <Main>
+                <Notes />
+            </Main>
+        )
+   
+}
---
+++ components/Notes.jsx
@@ -0,0 +1,11 @@
+import { useAuth } from "@/context/AuthContext";
+
+export default function Notes(){
+    // const  { currentUser } = useAuth()
+
+    return (
+        <div>
+            hi there from notes
+        </div>
+    )
+}
